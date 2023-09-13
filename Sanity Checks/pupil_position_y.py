f9269afaf9d6d15a60627d991f1f1cb5d8f76a95
shiracohen4 <107036604+shiracohen4@users.noreply.github.com>
2023-09-13 17:57:09 +0300

    delete unnecessary comments

--- Sanity Checks/pupil_position_y.py
+++ Sanity Checks/pupil_position_y.py
@@ -1,22 +1,3 @@
-# import pandas as pd
-# import matplotlib.pyplot as plt
-#
-# # Read the CSV file into a DataFrame
-# df = pd.read_csv('pupil_position_y_data.csv')
-#
-# # Remove the lines containing 'NEW SCENE'
-# df = df[pd.notna(df['Frame'])]
-#
-# # Plotting the data
-# plt.scatter(df['Frame'], df['Left Pupil Position y'], c='green', label='Left Pupil')
-# plt.scatter(df['Frame'], df['Right Pupil Position y'], c='orange', label='Right Pupil')
-# plt.xlabel('Frame')
-# plt.ylabel('Pupil Position Along Y Axis')
-# plt.title('Pupil Y Axis Comparison')
-# plt.legend()
-# plt.show()
-
-
 # scatter plot
 import pandas as pd
 import matplotlib.pyplot as plt
